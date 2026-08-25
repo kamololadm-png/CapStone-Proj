a36907c7c3c6f5877c78f3b8f461e3a3398fa707
kamololadm-png <kamololadm@gmail.com>
2026-08-25 14:36:33 +0200

    shop code

---
+++ airbnb-frontend/src/components/home/ShopAirbnb.jsx
@@ -0,0 +1,15 @@
+const ShopAirbnb = () => {
+  return (
+    <section className="shop-airbnb">
+      <div className="shop-airbnb-text">
+        <h2>Give the gift of travel</h2>
+        <button>Shop Airbnb gift cards</button>
+      </div>
+      <div className="shop-airbnb-image">
+        <img src="/images/gift-cards.jpg" alt="Airbnb gift cards" />
+      </div>
+    </section>
+  );
+};
+
+export default ShopAirbnb;
