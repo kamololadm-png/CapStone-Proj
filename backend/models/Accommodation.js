490e3bc34e7be4739905fab0b4922c4146694c1f
kamololadm-png <kamololadm@gmail.com>
2026-08-25 15:06:43 +0200

    update location page

--- backend/models/Accommodation.js
+++ backend/models/Accommodation.js
@@ -63,6 +63,17 @@ const accommodationSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    rating: {
+      type: Number,
+      default: 4.5,
+      min: 0,
+      max: 5,
+    },
+    reviews: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
     host: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
