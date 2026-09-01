3288d1df3e012de5b399327002ca3de36ed61a00
kamololadm-png <kamololadm@gmail.com>
2026-09-01 13:42:45 +0200

    image

--- airbnb-frontend/src/components/home/ShopAirbnb.jsx
+++ airbnb-frontend/src/components/home/ShopAirbnb.jsx
@@ -7,7 +7,7 @@ const ShopAirbnb = () => {
         <button>Shop Airbnb gift cards</button>
       </div>
       <div className="shop-airbnb-image">
-        <img src="/images/gift-cards.jpg" alt="Airbnb gift cards" />
+        <img src="https://plus.unsplash.com/premium_photo-1728501571457-bc5826622a58?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Z2lmdCUyMGNhcmR8ZW58MHx8MHx8fDA%3Ds" alt="Airbnb gift cards" />
       </div>
     </section>
   );
