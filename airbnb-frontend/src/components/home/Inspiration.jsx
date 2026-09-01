39748bb4904b4b524fff3652da6e820c11474227
kamololadm-png <kamololadm@gmail.com>
2026-09-01 13:32:04 +0200

    image

--- airbnb-frontend/src/components/home/Inspiration.jsx
+++ airbnb-frontend/src/components/home/Inspiration.jsx
@@ -1,8 +1,8 @@
 import "./Inspiration.css";
 const locations = [
-  { name: "New York", image: "https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?w=400&q=80" },
-  { name: "Paris", image: "https://images.unsplash.com/photo-1502602898ظف657-3e91760cbb34?w=400&q=80" },
-  { name: "Tokyo", image: "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?w=400&q=80" },
+  { name: "New York", image: "https://images.unsplash.com/photo-1541336032412-2048a678540d?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmV3JTIweW9ya3xlbnwwfHwwfHx8MA%3D%3D" },
+  { name: "Paris", image: "https://plus.unsplash.com/premium_photo-1661919210043-fd847a58522d?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFyaXN8ZW58MHx8MHx8fDA%3D" },
+  { name: "Tokyo", image: "https://plus.unsplash.com/premium_photo-1661914240950-b0124f20a5c1?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dG9reW98ZW58MHx8MHx8fDA%3D" },
   { name: "Cape Town", image: "https://images.unsplash.com/photo-1580060839134-75a50c3f7a37?w=400&q=80" },
 ];
 
