ab7e405e9e8081f6f899ae1f79a2361d58f43c40
kamololadm-png <kamololadm@gmail.com>
2026-09-10 15:53:54 +0200

    durban

--- airbnb-frontend/src/components/home/Inspiration.jsx
+++ airbnb-frontend/src/components/home/Inspiration.jsx
@@ -22,7 +22,7 @@ const locations = [
   {
     name: "KwaZulu-Natal",
     image:
-      "https://images.unsplash.com/photo-1515898698999-18f625d67499?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8a3dhenVsdSUyMG5hdGFsfGVufDB8fDB8fHww",
+      "https://images.unsplash.com/photo-1477901701740-5304fcfcb7fa?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZHVyYmFufGVufDB8fDB8fHww",
   },
   {
     name: "Johannesburg",
